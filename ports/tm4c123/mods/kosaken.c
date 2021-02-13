0cf5a8c26b37e8699900062a28aa7313ce85e168
dannyvondenhoerbergen <daniel.hoerber@st.oth-regensburg.de>
2021-02-13 15:34:39 +0100

    Test Module removed

--- ports/tm4c123/mods/kosaken.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void printit() {
-printf("Kosaken muessen reiten\n");
-}
-
--- ports/tm4c123/mpconfigport.h
+++ ports/tm4c123/mpconfigport.h
@@ -174,7 +174,6 @@ static inline mp_uint_t disable_irq(void) {
 extern const struct _mp_obj_module_t machine_module;
 extern const struct _mp_obj_module_t pyb_module;
 extern const struct _mp_obj_module_t mp_module_uos;
-extern const struct _mp_obj_module_t test_module;
 //extern const struct _mp_obj_module_t pin_module;
 
 // extra built in names to add to the global namespace
@@ -202,7 +201,6 @@ extern const struct _mp_obj_module_t test_module;
 #define MICROPY_PORT_BUILTIN_MODULES \
         { MP_ROM_QSTR(MP_QSTR_umachine), MP_ROM_PTR(&machine_module) }, \
         { MP_ROM_QSTR(MP_QSTR_pyb), MP_ROM_PTR(&pyb_module) }, \
-        { MP_ROM_QSTR(MP_QSTR_test), MP_ROM_PTR(&test_module) }, \
        { MP_ROM_QSTR(MP_QSTR_uos), MP_ROM_PTR(&mp_module_uos) }, 
 
 #define MICROPY_PORT_CONSTANTS \
